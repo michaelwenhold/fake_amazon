9c2cf6a68f7028b49205bb4b0e075f00b4e62b7e
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-10-25 23:47:49 -0500

    show list of products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,9 +16,34 @@ var connection = mysql.createConnection({
 });
 
 // Connect to MySQL
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
 
   // Start function after the connection is made
   start();
 });
+
+
+//Show list of products
+function start() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    console.log("Items for Sale");
+    
+    for (i=0; i<results.length; i++){
+      console.log(" Item # " + results[i].item_id + " is a " + results[i].product_name + " --- " + "The price is $" + results[i].price)
+    }
+  })
+
+
+//Prompt the user what action
+inquirer
+  .prompt([
+    {
+      name: "item_id",
+      type: "input",
+      message: "What product would you like to purchase. Enter the id number"
+
+    }
+  ])
+}
